cfa15ed9aab974dc7fe938ca32fcd24af6737625
Starikov Yaroslav <slava.com.com90@gmail.com>
2021-07-22 22:27:29 +0700

    fix: внести доработки по замечаниям ревью

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let popup = document.querySelector('.pop-up');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.pop-up__close-button');
+let Name = document.querySelector('.profile__name');
+let About = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.pop-up__input_name');
+let jobInput = document.querySelector('.pop-up__input_about');
+
+function open() {
+  popup.classList.add('pop-up_opened');
+  nameInput.value = Name.textContent;
+  jobInput.value = About.textContent;
+}
+
+function close() {
+  popup.classList.remove('pop-up_opened');
+}
+
+editButton.addEventListener('click', open);
+closeButton.addEventListener('click', close);
+
+let formElement = document.querySelector('.pop-up__form');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  Name.textContent = nameInput.value;
+  About.textContent = jobInput.value;
+  close();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
